f57cef96ad83c5311ae12d73406c06b9ebb80b02
nicolas <jnch2005@gmail.com>
2025-04-16 14:11:59 -0500

    Inicializacion proyecto

---
+++ proyectoestructuradatos/src/main/java/com/plataformaEducativa/proyectoestructuradatos/ProyectoestructuradatosApplication.java
@@ -0,0 +1,13 @@
+package com.plataformaEducativa.proyectoestructuradatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoestructuradatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoestructuradatosApplication.class, args);
+	}
+
+}
